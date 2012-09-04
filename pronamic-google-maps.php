ea3b34bca43057679d9b4ae9303ae94ff27b2195
Remco Tolsma <info@remcotolsma.nl>
2012-09-04 08:43:12 +0200

    WordPress Coding Standards optimizations.

--- pronamic-google-maps.php
+++ pronamic-google-maps.php
@@ -16,23 +16,23 @@ Domain Path: /languages/
 License: GPL
 */
 
-if(function_exists('spl_autoload_register')):
+if ( function_exists( 'spl_autoload_register' ) ) {
 
-function pronamic_google_maps_autoload($name) {
-	$name = str_replace( '\\', DIRECTORY_SEPARATOR, $name );
-	$name = str_replace( '_', DIRECTORY_SEPARATOR, $name );
-
-	$file = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . $name . '.php';
-
-	if ( is_file( $file ) ) {
-		require_once $file;
+	function pronamic_google_maps_autoload($name) {
+		$name = str_replace( '\\', DIRECTORY_SEPARATOR, $name );
+		$name = str_replace( '_', DIRECTORY_SEPARATOR, $name );
+	
+		$file = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . $name . '.php';
+	
+		if ( is_file( $file ) ) {
+			require_once $file;
+		}
 	}
+	
+	spl_autoload_register( 'pronamic_google_maps_autoload' );
+	
+	require_once 'functions.php';
+	
+	Pronamic_Google_Maps_Maps::bootstrap( __FILE__ );
+
 }
-
-spl_autoload_register( 'pronamic_google_maps_autoload' );
-
-require_once 'functions.php';
-
-Pronamic_Google_Maps_Maps::bootstrap( __FILE__ );
-
-endif;
