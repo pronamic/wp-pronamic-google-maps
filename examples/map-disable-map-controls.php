e500b00b90200bae195273c368b19c05fbdea3ea
Remco Tolsma <info@remcotolsma.nl>
2012-10-16 09:27:51 +0200

    Added disable map controls example.

---
+++ examples/map-disable-map-controls.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Template snippet
+ */
+if ( function_exists( 'pronamic_google_maps' ) ) {
+	pronamic_google_maps( array(
+		'width'       => 800,
+		'height'      => 800,
+		// https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+		'map_options' => array(
+			'mapTypeControl'    => false,
+			'navigationControl' => false,
+			'streetViewControl' => false,
+			'scrollwheel'       => false,
+			'zoomControl'		=> false			
+		)
+	) );
+}
