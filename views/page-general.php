53a631107c9e45b878b3e7074dcfa873749ea1b1
Remco Tolsma <info@remcotolsma.nl>
2013-06-03 14:14:39 +0200

    Nu longer use SLUG class constant, and renamed to ''pronamic_google_maps' (with underscores).

--- views/page-general.php
+++ views/page-general.php
@@ -1,5 +1,5 @@
 <div id="pgm" class="wrap">
-	<?php screen_icon( Pronamic_Google_Maps_Maps::SLUG ); ?>
+	<?php screen_icon( 'pronamic_google_maps' ); ?>
 
 	<h2><?php echo get_admin_page_title(); ?></h2>
 
