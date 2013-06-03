1c06fc2ac5f45218e30dd429b709ab728471bb43
Remco Tolsma <info@remcotolsma.nl>
2013-06-03 14:12:59 +0200

    Use get_admin_page_title().

--- views/page-general.php
+++ views/page-general.php
@@ -1,7 +1,7 @@
 <div id="pgm" class="wrap">
 	<?php screen_icon( Pronamic_Google_Maps_Maps::SLUG ); ?>
 
-	<h2><?php echo esc_html( __( 'Configuration - Pronamic Google Maps', 'pronamic_google_maps' ) ); ?></h2>
+	<h2><?php echo get_admin_page_title(); ?></h2>
 
 	<form action="options.php" method="post">
 		<?php settings_fields( 'pronamic_google_maps_settings' ); ?>
