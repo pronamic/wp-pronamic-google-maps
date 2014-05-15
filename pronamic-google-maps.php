d9f5737f95693bff88f516ca0f246cfedd217326
Remco Tolsma <info@remcotolsma.nl>
2014-05-15 14:39:31 +0200

    Update URI's.

--- pronamic-google-maps.php
+++ pronamic-google-maps.php
@@ -1,14 +1,14 @@
 <?php
 /*
 Plugin Name: Pronamic Google Maps
-Plugin URI: http://pronamic.eu/wordpress-plugins/pronamic-google-maps/
+Plugin URI: http://www.happywp.com/plugins/pronamic-google-maps/
 Description: This plugin makes it simple to add Google Maps to your WordPress post, pages or other custom post types.
 
 Version: 2.2.8
 Requires at least: 3.0
 
 Author: Pronamic
-Author URI: http://pronamic.eu/
+Author URI: http://www.pronamic.eu/
 
 Text Domain: pronamic_google_maps
 Domain Path: /languages/
