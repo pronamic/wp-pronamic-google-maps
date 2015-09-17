cda78ff7be917903fa1343384d62f54efa81816d
Remco Tolsma <info@remcotolsma.nl>
2015-09-17 12:40:09 +0200

    Updated documentation.

--- src/readme-md/overlapping-marker-spiderfier.php
+++ src/readme-md/overlapping-marker-spiderfier.php
@@ -1,6 +1,6 @@
 ## Overlapping Marker Spiderfier
 
-The Pronamic Google Maps plugin has builtin support for the [Overlapping Marker Spiderfier](https://github.com/jawj/OverlappingMarkerSpiderfier) library.
+The Pronamic Google Maps plugin has built-in support for the [Overlapping Marker Spiderfier](https://github.com/jawj/OverlappingMarkerSpiderfier) library.
 This library will be enabled if you pass in the `overlapping_marker_spiderfier_options` argument in the mashup arguments, see example below.
 
 ### Example
