9140f48253d54698dca081d79785bb09dc8cceb3
Reüel van der Steege <reuel@pronamic.nl>
2025-06-27 10:03:25 +0200

    Remove unnecessary check for `spl_autoload_register()` (available since PHP 5.1.2).

--- pronamic-google-maps.php
+++ pronamic-google-maps.php
@@ -18,23 +18,19 @@
  * License:           GPL
  */
 
-if ( function_exists( 'spl_autoload_register' ) ) {
+function pronamic_google_maps_autoload( $name ) {
+	$name = str_replace( '\\', DIRECTORY_SEPARATOR, $name );
+	$name = str_replace( '_', DIRECTORY_SEPARATOR, $name );
 
-	function pronamic_google_maps_autoload( $name ) {
-		$name = str_replace( '\\', DIRECTORY_SEPARATOR, $name );
-		$name = str_replace( '_', DIRECTORY_SEPARATOR, $name );
+	$file = plugin_dir_path( __FILE__ ) . 'classes' . DIRECTORY_SEPARATOR . $name . '.php';
 
-		$file = plugin_dir_path( __FILE__ ) . 'classes' . DIRECTORY_SEPARATOR . $name . '.php';
-
-		if ( is_file( $file ) ) {
-			require_once $file;
-		}
+	if ( is_file( $file ) ) {
+		require_once $file;
 	}
-
-	spl_autoload_register( 'pronamic_google_maps_autoload' );
-
-	require_once 'includes/functions.php';
-
-	Pronamic_Google_Maps_Maps::bootstrap( __FILE__ );
-
 }
+
+spl_autoload_register( 'pronamic_google_maps_autoload' );
+
+require_once 'includes/functions.php';
+
+Pronamic_Google_Maps_Maps::bootstrap( __FILE__ );
